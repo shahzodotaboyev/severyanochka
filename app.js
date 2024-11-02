96dbe82bce92e9d3651e0c3388e4229779697fbf
shahzodotaboyev <shoxzodbek15@gmail.com>
2024-11-02 14:10:50 +0300

    show

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ let body = document.body;
 const darkmode = () => {
 body.classList.toggle('darkMode')
 }   
+
+function qotologmnElements(){
+    let qotologmn = document.getElementsByClassName('qotolog')[0]
+    qotologmn.classList.toggle('block')
+}
